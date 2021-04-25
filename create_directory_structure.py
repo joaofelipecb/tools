7d794f1135e8d28a2362323a79c382f447cd0c29
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-04-25 11:28:50 -0300

    create directory structure

---
+++ create_directory_structure.py
@@ -0,0 +1,6 @@
+import os
+
+directories = ['p17data','p18test','p19version','p20branch','p21container','p22enviroment','p23control','p24command','p25interface','p26struct','p27develop','p28except']
+
+for directory in directories:
+    os.mkdir(directory)
