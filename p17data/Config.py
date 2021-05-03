ddc9068fb0ebaee2ed474b34bd32a72002f7f2ab
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-05-03 13:19:19 -0300

    create separeted file for security data

--- p17data/Config.py
+++ p17data/Config.py
@@ -1,3 +1,9 @@
+import tools.p17data.ConfigSecurity
 name = 'Tools'
 version = '0.0.0.1.4'
+database = {}
+database['dbms'] = 'postgress'
+database['host'] = tools.p17data.ConfigSecurity.database['host']
+database['user'] = tools.p17data.ConfigSecurity.database['user']
+database['password'] = tools.p17data.ConfigSecurity.database['password']
 
